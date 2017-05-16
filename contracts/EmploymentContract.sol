a916dee7860ef49a03cc90d5f8075af127a16569
sandhya <c.sandhya@capitalfloat.com>
2017-05-16 21:41:29 +0530

    partial commit

--- contracts/EmploymentContract.sol
+++ contracts/EmploymentContract.sol
@@ -5,21 +5,28 @@ contract EmploymentContract {
     struct Terms {
         uint dailySalary;
         uint contractDays;
+        uint numRatings;
+        mapping (uint => uint) ratings; 
     }
 
     mapping (address => Terms) employmentContracts;
 
     mapping (address=>uint) balances;
 
+    address public escrow;
+
     address public employer;
 
     function EmploymentContract() {
         employer = msg.sender;
         balances[msg.sender] = 10000;
+        escrow = 0xfb230af80ac1b1df8219581d870c10ef2a2af8be;
     }
 
-    function addContract(address employee, uint amount, uint contractDays) {
-        employmentContracts[employee] = Terms(amount, contractDays);
+    function addContract(address employee, uint amount, uint contractDays) returns(bool added) {
+        var totalAmount = amount * contractDays;
+        if(!sendCoin(escrow,totalAmount)) return false;
+        employmentContracts[employee] = Terms(amount, contractDays,0);
     }
 
     function removeContract(address employee) {
@@ -37,12 +44,30 @@ contract EmploymentContract {
 		return true;
 	}
 
+    function genericSendCoin(address sender, address receiver, uint amount) returns(bool sufficient) {
+		if (balances[sender] < amount) return false;
+		balances[sender] -= amount;
+		balances[receiver] += amount;
+		return true;
+	}
+
     function getBalance(address addr) returns(uint) {
   	    return balances[addr];
 	}
 
     function checkBalance(address addr) returns(uint balance) {
         return balances[addr];
+    }
+
+    function addRating(address employeeAddr, uint rating) returns (uint) {
+        employmentContracts[employeeAddr].ratings[employmentContracts[employeeAddr].numRatings] = rating;
+        genericSendCoin(escrow, employeeAddr, employmentContracts[employeeAddr].dailySalary);
+        employmentContracts[employeeAddr].numRatings++;
+        return employmentContracts[employeeAddr].numRatings;
+    }
+
+    function getRatingNum(address addr) returns(uint ratingNum) {
+        return employmentContracts[addr].numRatings;
     }
 
 }
---
+++ contracts/EmploymentContractFactory.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.2;
+
+import "./EmploymentContract.sol";
+
+contract EmploymentContractFactory {
+
+    mapping (address => EmploymentContract) contracts;
+
+    
+
+
+}
